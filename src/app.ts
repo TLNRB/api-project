037811305d09ac4361f354acb485421c24febd48
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-01 10:25:11 +0100

    dotEnv created with port information, port info loaded from there

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import routes from './routes';
 import dotenvFlow from 'dotenv-flow';
 
-/* dotenvFlow.config(); */
+dotenvFlow.config();
 
 // Creating express application
 const app: Application = express();
@@ -10,7 +10,8 @@ const app: Application = express();
 app.use('/api', routes);
 
 export function startServer() {
-   app.listen(4000, function () {
-      console.log('Server is running on port 4000');
+   const PORT: number = parseInt(process.env.PORT as string) || 4000;
+   app.listen(PORT, function () {
+      console.log('Server is running on port: ', PORT);
    })
 }
