451c1e9f1dc431ec0f1540f079cfd9645b81253a
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-01 10:03:20 +0100

    get route created and routes imported

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import routes from './routes';
 import dotenvFlow from 'dotenv-flow';
 
-dotenvFlow.config();
+/* dotenvFlow.config(); */
 
 // Creating express application
 const app: Application = express();
+
+app.use('/api', routes);
 
 export function startServer() {
    app.listen(4000, function () {
