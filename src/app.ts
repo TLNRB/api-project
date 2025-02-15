364b5396abcad7aba6bc17456ed049c1a4a4be5d
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-15 10:53:50 +0100

    create route added to router

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,14 @@ dotenvFlow.config();
 // Creating express application
 const app: Application = express();
 
-app.use('/api', routes);
-
 export function startServer() {
+   // Binding routes to the app
+   app.use('/api', routes);
+
+   // Testing connection to the database
    testConnection();
 
+   // Starting the server
    const PORT: number = parseInt(process.env.PORT as string) || 4000;
    app.listen(PORT, function () {
       console.log('Server is running on port: ', PORT);
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express';
+import { createGame } from './controllers/gameController';
 
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
    res.status(200).send('Hello there!');
 });
+
+router.post('/games', createGame);
 
 export default router;
