e1f360226ff5e66f7a9af9231fea7ef71cfa9e7b
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-22 11:35:00 +0100

    register user route added

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { createGame, getAllGames, getGameById, updateGameById, deleteGameById } from './controllers/gameController';
 import { create } from 'domain';
+import { registerUser } from './controllers/authController';
 
 const router: Router = Router();
 
@@ -8,10 +9,18 @@ router.get('/', (req: Request, res: Response) => {
    res.status(200).send('Hello there!');
 });
 
+// Create a new game
 router.post('/games', createGame);
+// Get all games
 router.get('/games', getAllGames);
+// Get a game by ID
 router.get('/games/:id', getGameById);
+// Update a game by ID
 router.put('/games/:id', updateGameById);
+// Delete a game by ID
 router.delete('/games/:id', deleteGameById);
+
+// Authentication
+router.post('/user/register', registerUser);
 
 export default router;
