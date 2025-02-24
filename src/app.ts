eb8ca44c06e4ca9ab138fa7a98fb0fe017a40583
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-24 10:13:16 +0100

    cors code snippets added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,38 @@
 import express, { Application, Request, Response } from 'express';
 import routes from './routes';
 import dotenvFlow from 'dotenv-flow';
-import { connect, disconnect, testConnection } from './repository/database';
-import test from 'node:test';
+import { testConnection } from './repository/database';
+import cors from 'cors';
 
 dotenvFlow.config();
 
 // Creating express application
 const app: Application = express();
 
+export function setupCors() {
+   app.use(
+      cors({
+         origin: "*", // Allow requests from any origin
+         methods: 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE',
+         allowedHeaders: ['auth-token', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'], // Allow specific headers
+         credentials: true,
+      })
+   );
+   // set the Access-Control-Allow-Origin header for preflight requests
+   /* app.options('*', (req: Request, res: Response) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'auth-token, Origin, X-Requested-With, Content-Type, Accept');
+      // test for credentials
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.sendStatus(200);
+   }); */
+}
+
 export function startServer() {
+   // Setting up CORS
+   setupCors();
+
    // JSON body parser
    app.use(express.json());
 
